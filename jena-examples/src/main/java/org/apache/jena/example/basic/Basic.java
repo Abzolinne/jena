887a709c27bc7c6c655d42cba4c0bce0e3284df3
ernesto tapia <62125430+Abzolinne@users.noreply.github.com>
2025-11-15 21:42:57 -0300

    basic example

---
+++ jena-examples/src/main/java/org/apache/jena/example/basic/Basic.java
@@ -0,0 +1,50 @@
+package org.apache.jena.example.basic;
+
+import org.apache.jena.query.*;
+import org.apache.jena.rdf.model.*;
+import org.apache.jena.util.FileManager;
+
+import java.io.File;
+import java.io.InputStream;
+
+public class Basic {
+    public static void main(String[] args) {
+
+        Model model = ModelFactory.createDefaultModel();
+        String resourcePath = "src/main/resources/jena/examples/basic/data.ttl";
+        System.out.println("📁 Ruta real del TTL: " + new File(resourcePath).getAbsolutePath());
+        InputStream in = FileManager.getInternal().open(resourcePath);
+        if (in == null) {
+            throw new IllegalArgumentException("Archivo TTL no encontrado: " + resourcePath);
+        }
+
+        model.read(in, null, "TTL");
+
+        System.out.println("📦 Modelo cargado:");
+        model.write(System.out, "TTL");
+
+        System.out.println("\n🔍 Resultado de la consulta SPARQL con SIZE():");
+
+        String queryStr = """
+                PREFIX ex: <http://example.org/>
+				PREFIX sim: <http://sim.dcc.uchile.cl/>
+				
+				SELECT ?person ?vector ?size
+				WHERE {
+				  ?person sim:hasVector ?vector .
+				  BIND(SIZE(?vector) AS ?size)
+				  FILTER(?size > 3)
+				}
+				ORDER BY DESC(?size)
+            """;
+
+        Query query = QueryFactory.create(queryStr, Syntax.syntaxSPARQL_11_sim);
+        try (QueryExecution qexec = QueryExecutionFactory.create(query, model)) {
+            ResultSet results = qexec.execSelect();
+            ResultSetFormatter.out(System.out, results, query);
+        } catch (Exception e) {
+            System.err.println("❌ Error ejecutando la consulta: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
